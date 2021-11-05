1f7637841bbac44777f990f117937fcb4a303ea4
David Schonberger <david@sonosim.com>
2021-11-04 19:52:04 -0700

    implement xpath scraper spider script

--- mec-5.5.4-webscraping-project/scrapy_mini_project/scrapy_mini_project/spiders/quotes_spider_xpath_scrape.py
+++ mec-5.5.4-webscraping-project/scrapy_mini_project/scrapy_mini_project/spiders/quotes_spider_xpath_scrape.py
@@ -1,7 +1,7 @@
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "quotes2"
 
     start_urls = [
     'http://quotes.toscrape.com/page/1/',
@@ -10,10 +10,10 @@ class QuotesSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
+        for quote in response.xpath('//div[@class="quote"]'):
             yield{
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall()
+                'text': quote.xpath('//span[@class="text"]/text()').get(),
+                'author': quote.xpath('//small[@class="author"]/text()').get(),
+                'tags': quote.xpath('./div[@class="tags"]/a[@class="tag"]/text()').getall()
             }
-            yield from response.follow_all(css='li.next a', callback=self.parse)
+            yield from response.follow_all(xpath='//li[@class="next"]/a', callback=self.parse)
